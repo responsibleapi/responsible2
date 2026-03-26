51e18ad3af52e18461ab77a8406db2370ed91763
adelnizamutdinov <stiggpwnz@gmail.com>
2026-03-26 11:51:38 +0300

    amen

--- src/examples/youtube.test.ts
+++ src/examples/youtube.test.ts
@@ -1,14 +1,14 @@
 import { describe, expect, test } from "vitest"
 import { validate } from "../validate.ts"
 import json from "./youtube.json"
-// import api from "./youtube.ts"
+import api from "./youtube.ts"
 
 describe("youtube example", () => {
   test("youtube.json is valid", async () => {
     expect(await validate(json)).toEqual(json)
   })
 
-  // test("youtube.json validates as OpenAPI", async () => {
-  //   expect(await validate(api)).toEqual<typeof json>(json)
-  // })
+  test("youtube.json validates as OpenAPI", async () => {
+    expect(await validate(api)).toEqual<typeof json>(json)
+  })
 })
