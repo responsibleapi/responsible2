1b393033df79659c35bb5ef91a92912fee80128a
adelnizamutdinov <stiggpwnz@gmail.com>
2026-04-18 08:37:58 +0300

    amen

--- src/examples/pachca.test.ts
+++ src/examples/pachca.test.ts
@@ -1,9 +1,18 @@
+import type { oas31 } from "openapi3-ts"
 import { describe, expect, test } from "vitest"
+import { normalize } from "../help/normalize.ts"
 import { validateDoc } from "../help/validate-doc.ts"
 import theJSON from "./pachca.json"
+import theAPI from "./pachca.ts"
 
 describe("pachca", () => {
-  test("pachca.json is valid", async () => {
+  test("json is valid", async () => {
     expect(await validateDoc(theJSON)).toEqual(theJSON)
+  })
+
+  test("compiles to json", async () => {
+    expect(normalize(await validateDoc(theAPI))).toEqual(
+      normalize(theJSON as oas31.OpenAPIObject),
+    )
   })
 })
